4487232a1af81c67a46cd99c2ed6d5352ecab930
Aya <31237389+tal5@users.noreply.github.com>
2024-01-08 17:07:06 +0000

    Remove `IMetaTypeData` getters

--- SharpDenizenTools/MetaObjects/MetaTypeData.cs
+++ SharpDenizenTools/MetaObjects/MetaTypeData.cs
@@ -9,19 +9,8 @@ namespace SharpDenizenTools.MetaObjects
     /// <summary>A generic-less <see cref="MetaTypeData{T}"/> interface.</summary>
     public interface IMetaTypeData
     {
-        /// <summary>Getter to create a new instance of this meta type.</summary>
-        Func<MetaObject> Getter { get; }
-
-        /// <summary>The meta type this data is for.</summary>
-        MetaType Type { get; }
-
-        /// <summary>Creates a new <see cref="MetaObject"/> using <see cref="Getter"/> and sets it's type.</summary>
-        public MetaObject CreateNewMeta()
-        {
-            MetaObject newMeta = Getter();
-            newMeta.Type = Type;
-            return newMeta;
-        }
+        /// <summary>Creates a new <see cref="MetaObject"/> and sets its type.</summary>
+        MetaObject CreateNewMeta();
 
         /// <summary>Gets a <see cref="MetaObject"/> by name, or <c>null</c> if one by that name doesn't exist.</summary>
         MetaObject MetaByNameIfPresent(string name);
@@ -33,8 +22,13 @@ namespace SharpDenizenTools.MetaObjects
     /// <summary>Data for a specific <see cref="MetaType"/>.</summary>
     public record MetaTypeData<T>(Dictionary<string, T> Meta, Func<T> Getter, MetaType Type) : IMetaTypeData where T : MetaObject
     {
-        Func<MetaObject> IMetaTypeData.Getter => Getter;
-        MetaType IMetaTypeData.Type => Type;
+        /// <inheritdoc/>
+        public MetaObject CreateNewMeta()
+        {
+            MetaObject newMeta = Getter();
+            newMeta.Type = Type;
+            return newMeta;
+        }
 
         /// <inheritdoc/>
         public MetaObject MetaByNameIfPresent(string name)
