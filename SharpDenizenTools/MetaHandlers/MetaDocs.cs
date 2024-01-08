6381d800d0c46ff0c70a1a8b419a91e26e8bb929
Aya <31237389+tal5@users.noreply.github.com>
2024-01-08 14:11:06 +0000

    Doc fix

--- SharpDenizenTools/MetaHandlers/MetaDocs.cs
+++ SharpDenizenTools/MetaHandlers/MetaDocs.cs
@@ -98,7 +98,7 @@ namespace SharpDenizenTools.MetaHandlers
         /// <summary>Set of raw adjustable keys.</summary>
         public HashSet<string> RawAdjustables = [];
 
-        /// <inheritdoc/>
+        /// <summary>Creates a new instance of <see cref="MetaDocs"/> and registers it's <see cref="MetaTypeData{T}"/>.</summary>
         public MetaDocs()
         {
             // Extensions explicitly first
