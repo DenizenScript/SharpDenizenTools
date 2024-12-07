c7c71f1cfb570ba6b19000ca2cbff20649de242b
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-12-07 09:03:22 -0800

    add suppression for ms being dumb

--- SharpDenizenTools/GlobalSuppressions.cs
+++ SharpDenizenTools/GlobalSuppressions.cs
@@ -8,3 +8,4 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible")]
 [assembly: SuppressMessage("Style", "IDE0074:Use compound assignment", Justification = "awkward syntax")]
 [assembly: SuppressMessage("Performance", "CA1860:Avoid using 'Enumerable.Any()' extension method")]
+[assembly: SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "wtf")]
